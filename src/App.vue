cb826501547017c2b283bc8c88fab4671154c623
quyhp <tuyhpq@gmail.com>
2020-12-10 01:09:18 +0700

    use admin-lte

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,82 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+    </ul>
+  </nav>
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <router-link :to="{ name: 'Home' }" class="brand-link">
+      <img src="@/assets/logo.png" class="brand-image img-circle elevation-3" />
+      <span class="brand-text font-weight-light">FBVN</span>
+    </router-link>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="@/assets/logo.png" class="img-circle elevation-2" />
+        </div>
+        <div class="info">
+          <router-link :to="{ name: 'Home' }" class="d-block">Hứa Phú Quý</router-link>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <li class="nav-item">
+            <router-link :to="{ name: 'Home' }" class="nav-link" active-class="active">
+              <i class="nav-icon far fa-image"></i>
+              <p>Lọc bạn bè</p>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'Home' }" class="nav-link" active-class="active">
+              <i class="nav-icon far fa-image"></i>
+              <p>Lọc bạn bè theo tương tác</p>
+            </router-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Dashboard</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Main content -->
+    <section class="content">
+      <router-view />
+    </section>
   </div>
-  <router-view />
+
+  <!-- Footer -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block"><b>Version</b> 3.1.0-rc</div>
+  </footer>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  font-family: Helvetica, Arial, sans-serif;
 }
 </style>
---
+++ src/plugins/admin-lte.ts
@@ -0,0 +1,3 @@
+import 'admin-lte/dist/css/adminlte.min.css';
+import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js';
+import 'admin-lte/dist/js/adminlte.min.js';
